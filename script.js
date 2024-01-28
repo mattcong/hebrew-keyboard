90cfa78a160c5b6fdf367c2f08abb2ca60cbc23d
Matt Congdon <80398444+mattcong@users.noreply.github.com>
2024-01-28 12:24:25 +0000

    add backspace / shift / space keys

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-document.querySelectorAll(".key").forEach(function (key) {
+document.querySelectorAll(".letter, .space").forEach(function (key) {
   key.addEventListener("click", function () {
     const keyValue = key.textContent
     const textInput = document.getElementById("input")
     textInput.value += keyValue
   })
+})
+
+document.getElementById("backspaceKey").addEventListener("click", function () {
+  const textInput = document.getElementById("input")
+  textInput.value = textInput.value.slice(0, -1)
 })
